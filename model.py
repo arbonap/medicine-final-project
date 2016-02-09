0f3fc2b85228ae5e93899d63dc15d1d06737105e
Hackbright Student <info@hackbrightacademy.com>
2016-02-08 18:05:27 -0800

    updated classes

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -11,7 +12,7 @@ class User(db.Model):
 
 	__tablename__ = "users"
 
-	id = db.Column(db.Integer, primary_key=True, nullable=False) #connect this to prescription
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False) 
 	name = db.Column(db.String(100), nullable=True)
 	condition = db.Column(db.String(150), nullable=True)
 
@@ -23,14 +24,20 @@ class User(db.Model):
 
 class Prescription(db.Model):
 
-	id = db.Column(db.Integer, primary_key=True, nullable=False)
+	__tablename__ = "prescriptions"
+
+	id = db.Column(db.Integer, primary_key=True, ForeignKey('users.id'), autoincrement=True, nullable=False)
 	med_name = db.Column(db.String(200), nullable=False)
-	side_effects = db.Column(db.String(200), nullable=True) #I need to make a new table for side_effects
+	side_effects = db.Column(db.String(200), nullable=True)
 	starting_amount = db.Column(db.Integer, nullable=True) #starting amount of medicine
-	refills_remaining = db.Column(db.Integer, nullable=True) #is there a way to autodecrement? must have a starting # of refills though..
+	refills_remaining = db.Column(db.Integer, nullable=True) 
 	black_box_warning = db.Column(db.String(300), nullable=True)
 	dosage = db.Column(db.String(300), nullable=False)
-	#include foreign key user id
+	user_id = foreign id#include user id as foreign key
+	doctor_id = foreign id
+	food = db.Column(db.Boolean), nullable=True) #food?
+	water = db.Column(db.Boolean), nullable=True) #water?
+
 
 	def __repr__(self):
 		"""Provides helpful representation data when printed for debugging purposes."""
@@ -39,11 +46,13 @@ class Prescription(db.Model):
 
 class Schedule(db.Model):
 
-	id = db.Column(db.Integer, primary_key=True, nullable=False)
-	prescription_id = db.Column(db.string(200), nullable=False) #Foreign key !!connect to id in Prescription class
-	date_started = db.Column(pass) #??
+	__tablename__ = "schedules"
+
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+
+	date_started = db.Column(pass) #??look up datetime object stuff
 	date_time = db.Column(pass) #??  must convert to datetime object
-	food_water = db.Column(db.Boolean(200), nullable=True) #food and or water requirements: find boolean
+	
 
 
 	def __repr__(self):
@@ -51,29 +60,17 @@ class Schedule(db.Model):
 
 		return "<Schedule information: >" % ()
 
-class SideEffects(db.Model):
+class Doctor(db.Model):
+	"""Doctor information"""
 
-	id = db.Column(db.Integer, primary_key=True, nullable=False)
+	__tablename____ = "doctors"
+	doctor_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False))
+	doctor_name = db.Column(db.String(100), primary_key=True, nullable=False) #not unique because I technically can have doctors with the same name?
+	condition = db.Column(db.String(300), nullable=True) #for what specific condition does the user see the doctor for? #foreignKey perhaps?
+	phone = db.Column(db.String(20), nullable=True) #should I make this phone number a string? or Integer?
+	office_address = db.Column(db.String(200), nullable=True)
 
+	def __repr__(self):
+		"""Provides helpful representation data when printed for debugging purposes."""
 
-
-# class Doctor(db.Model):
-# 	"""Doctor information"""
-
-# 	__tablename____ = "doctors"
-
-# 	doctor_name = db.Column(db.String(100), primary_key=True, nullable=False) #not unique because I technically can have doctors with the same name?
-# 	specialty = db.Column(db.String(100), nullable=True)
-# 	condition = db.Column(db.String(300), nullable=True) #for what specific condition does the user see the doctor for? #foreignKey perhaps?
-# 	gender = db.Column(db.String(50), nullable=True)
-# 	age = db.Column(db.Integer, nullable=True)
-# 	board_certification = db.Column(db.Boolean) #is this correct for making Boolean values?
-# 	hospital_affiliation = db.Column(db.String(250), nullable=True)
-# 	private_practice_affiliation = db.Column(db.String(250), nullable=True)
-# 	phone = db.Column(db.String(20), nullable=True) #should I make this phone number a string? or Integer?
-# 	office_address = db.Column(db.String(200), nullable=True)
-
-	# def __repr__(self):
-	# 	"""Provides helpful representation data when printed for debugging purposes."""
-
-	# 	return "<Doctor information: >" % (self.doctor_name, self.specialty, self.phone, self.office_address)
+		return "<Doctor information: >" % (self.doctor_name, self.specialty, self.phone, self.office_address)
