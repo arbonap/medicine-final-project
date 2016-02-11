928afdbd9a2c4791dbe6a53e9428e51ac3eb8c3d
U-DESKTOP-IBQHDGH\Patricia <arbonap@gmail.com>
2016-02-11 13:31:18 -0800

    set up index.html and registration.html

--- model.py
+++ model.py
@@ -9,18 +9,21 @@ db = SQLAlchemy()
 
 class User(db.Model):
     """User of medicine-tracking app.
-    Contains user_id, name and any diagnosed conditions."""
+    Contains user_id, name and ."""
 
     __tablename__ = "users"
 
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = db.Column(db.String(100), nullable=True)
-    condition = db.Column(db.String(150), nullable=True)
+    phone = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         """Provides helpful representation data when printed for debugging purposes."""
 
-        return "<User information:>" % ()
+        return "<User ID user_id=%s phone phone=%s password password=%s email email=%s first_name first_name=%s last_name last_name=%s>" % (self.user_id, self.phone, self.password, self.email, self.first_name, self.last_name)
 
 
 class Prescription(db.Model):
@@ -28,6 +31,7 @@ class Prescription(db.Model):
     __tablename__ = "prescriptions"
 
     prescription_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    reason = db.Column(db.String(150), nullable=True)
     med_name = db.Column(db.String(200), nullable=False)
     side_effects = db.Column(db.String(200), nullable=True)
     starting_amount = db.Column(db.Integer, nullable=True)
@@ -35,16 +39,16 @@ class Prescription(db.Model):
     refills_remaining = db.Column(db.Integer, nullable=True)
     black_box_warning = db.Column(db.String(300), nullable=True)
     dosage = db.Column(db.String(300), nullable=False)
+    food = db.Column(db.Boolean, nullable=True)
+    #food?
+    drink = db.Column(db.Boolean, nullable=True)
+    #water?
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     #ForeignKey connecting prescription and user
     doctor_id = db.Column(db.Integer, db.ForeignKey('doctors.doctor_id'))
     #ForeignKey connecting prescription and doctor
     schedule_id = db.Column(db.Integer, db.ForeignKey('schedules.schedule_id'))
     #ForeignKey connectiong prescription and schedule
-    food = db.Column(db.Boolean, nullable=True)
-    #food?
-    drink = db.Column(db.Boolean, nullable=True)
-    #water?
 
     #Define a relationship to user
     user = db.relationship("User", backref=db.backref("prescriptions"), order_by=prescription_id)
@@ -58,7 +62,7 @@ class Prescription(db.Model):
     def __repr__(self):
         """Provides helpful representation data when printed for debugging purposes."""
 
-        return "<Prescription information: >" % ()
+        return "<Prescription id prescription_id=%s reason reason=%s med_name med_name=%s side_effects side_effects=%s starting_amount starting_amount=%s refills_remaining refills_remaining=%s black_box_warning black_box_warning=%s dosage dosage=%s food food=%s drink drink=%s>" % (self.prescription_id, self.reason, self.med_name, self.side_effects, self.starting_amount, self.refills_remaining, self.black_box_warning, self.dosage, self.food, self.drink)
 
 
 class Schedule(db.Model):
@@ -69,19 +73,15 @@ class Schedule(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     prescription_id = db.Column(db.Integer, db.ForeignKey('prescriptions.prescription_id'))
     timestamp = db.Column(db.DateTime)
-    
     #define  relationship to users
     user = db.relationship("User", backref=db.backref("schedules"), order_by=schedule_id)
 
     #define a relationship to prescriptions
     prescription = db.relationship("Prescription", backref=db.backref("schedules"), order_by=schedule_id)
-    #request.args.getlist from a checklist form. that checklist will contian little boxes
-    #list should give backa list of times
-    #each item from the checklist is its own row
 
     def __repr__(self):
         """Provides helpful representation data when printed for debugging purposes."""
-        return "<Schedule information: >" % ()
+        return "<Schedule_id schedule_id=%s user_id user_id=%s prescription_id=%s timestamp timestamp=%s>" % (self.schedule_id, self.user_id, self.prescription_id, self.timestamp)
 
 
 class Doctor(db.Model):
@@ -90,25 +90,27 @@ class Doctor(db.Model):
     __tablename__ = "doctors"
 
     doctor_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    doctor_name = db.Column(db.String(100), primary_key=True, nullable=False)
+    doctor_name = db.Column(db.String(100), nullable=False)
     condition = db.Column(db.String(300), nullable=True)
     #for what specific condition does the user see the doctor for?
     phone = db.Column(db.String(20), nullable=True)
     #should I make this phone number a string? or Integer?
-    office_address = db.Column(db.String(200), nullable=True)
+    office_address = db.Column(db.String(300), nullable=True)
 
     def __repr__(self):
         """Provides helpful representation data when printed for debugging purposes."""
-        return "<Doctor information: >" % ()
+        return "<doctor_id doctor_id=%s doctor_name doctor_name=%s condition condition=%s phone phone=%s office_address=%s>" % (self.doctor_id, self.doctor_name, self.condition, self.phone, self.office_address)
+
 
 ##############################################################################
 # Helper functions
+
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
 
     # Configure to use our PostgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///medicines'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://patricia:hackbright@localhost/medicines'
     #name your database medicines??
 #    app.config['SQLALCHEMY_ECHO'] = True
     db.app = app
--- server.py
+++ server.py
@@ -1,23 +1,63 @@
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
-from model import db, User, Prescription, Schedule, Doctor
+from model import connect_to_db, db, User, Prescription, Schedule, Doctor
 
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
+#???
 
 # Normally, if you use an undefined variable in Jinja2, it fails silently.
 # This is horrible. Fix this so that, instead, it raises an error.
 app.jinja_env.undefined = StrictUndefined
+
 
 @app.route('/')
 def index():
     """Homepage."""
     return render_template("homepage.html")
 
-    
+    #request.args.getlist from a checklist form. that checklist will contian little boxes
+    #list should give back a list of times
+    #each item from the checklist is its own row
+
+
+@app.route('/register')
+def something():
+    """Sends user to fill out medicine information"""
+
+    return render_template("register.html")
+
+
+# @app.route('/pass', methods=["PASS"])
+# def something():
+#     """Something"""
+
+#     return render_template("pass")
+
+# @app.route('/pass', methods=["PASS"])
+# def something():
+#     """Something"""
+
+#     return render_template("pass")
+
+
+# @app.route('/pass', methods=["PASS"])
+# def something():
+#     """Something"""
+
+#     return render_template("pass")
+
+
+# @app.route('/pass', methods=["PASS"])
+# def something():
+#     """Something"""
+
+#     return render_template("pass")
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
