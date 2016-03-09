b1b27e4dd62f4ed8bad0aa6af7eb9125f6493c19
Patricia <Patricia@Patricias-MacBook-Air.local>
2016-03-08 17:40:22 -0800

    turned off the debugger

--- server.py
+++ server.py
@@ -369,7 +369,7 @@ if __name__ == "__main__":
     # that we invoke the DebugToolbarExtension
 
     # Do not debug for demo
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app, 'postgresql:///medicines')
 
